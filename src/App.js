cb8054c475f2eb9963e6ee2bc20ec8c550dcf100
Ruben David <rupedavecoo25@gmail.com>
2023-05-09 12:36:45 -0400

    Proyecto inicializado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import Logo from './imagenes/descarga.jfif'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      React
+    <div className='aplicacion-tareas'>
+      
+      <div className='freecodecamp-logo-contenedor'>
+      
+        <img src = {Logo} 
+        className='freecodecamplogo' 
+        alt='logo de freeCodeCamp' />
+      
+      </div>
+    
     </div>
   );
 }
