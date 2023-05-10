36046bd6d9342714f2228b98a0646532293149de
Ruben David <rupedavecoo25@gmail.com>
2023-05-10 15:01:22 -0400

    Implementando las tareas en appjs

--- src/componentes/Lista-de-tareas.js
+++ src/componentes/Lista-de-tareas.js
@@ -8,13 +8,16 @@ function ListaDeTareas(){
     const [tareas,setTareas] = useState([]);
 
     const agregarTarea = tarea => {
-        console.log("Tarea agregada");
-        console.log(tarea);
+        if (tarea.texto.trim()){
+            tarea.texto = tarea.texto.trim();
+            const tareaActualizada = [tarea,...tareas];
+        }
     }
 
     return(
         <>
-            <TareaFormulario />
+            <TareaFormulario 
+            onSubmit={agregarTarea}/>
             <div className="tarea-lista-contenedor">
                 {
                     tareas.map((tarea) =>
